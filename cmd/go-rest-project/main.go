d6e146696fd730304431748db22393d67fd369c8
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-09-26 23:23:21 +0530

    Changed the server setup and logging in main.go; fixed config struct tags in config.go

--- cmd/go-rest-project/main.go
+++ cmd/go-rest-project/main.go
@@ -1,31 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	// "fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/iamrohitkandpal/Go-REST-Project/internal/config"
+	"github.com/iamrohitkandpal/Go-REST-Project/internal/http/handlers/student"
 )
 
 func main() {
+	// Loading Config
 	cfg := config.MustLoad()
 
+
+	// Router Setup
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcom to project api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
+
+	// Server Setup
 	server := http.Server{
 		Addr:    cfg.Address,
 		Handler: router,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Failed to Run Server")
+	slog.Info("Server Started", slog.String("At Address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func (){
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to Run Server")
+		}
+	}()
+
+	<-done 
+
+	slog.Info("Shutting down the server")
+
+	ctx ,cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to Shutdown Server", slog.String("Error", err.Error()))
 	}
 
-	fmt.Println("Server Started")
+	slog.Info("Server SHutdown Successfully")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,14 +9,14 @@ import (
 )
 
 type HttpServer struct {
-	Address string
+	Address string `yaml:"address" env-required:"true"`
 }
 
 // env-default:"production"
 type Config struct {
-	Env        string `yaml:"env" env: "ENV" env-required: "true"`
-	StragePath string `yaml: storage_path env-required: "true"`
-	HttpServer `yaml: http_server`
+	Env        string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HttpServer `yaml:"http_server"`
 }
 
 func MustLoad() *Config {
