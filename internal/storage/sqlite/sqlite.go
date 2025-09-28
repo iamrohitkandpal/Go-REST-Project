b5d51668d16375026cc8844fd260518535ddd216
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-09-28 23:11:02 +0530

    Made SQLite storage implementation to include student creation and retrieval methods; Updated Student struct with JSON tags

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,8 +2,12 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
+
+	"github.com/iamrohitkandpal/Go-REST-Project/internal/types"
 
 	"github.com/iamrohitkandpal/Go-REST-Project/internal/config"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Sqlite struct {
@@ -15,4 +19,97 @@ func New(cfg config.Config) (*Sqlite, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS students (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT,
+	email TEXT,
+	age INTEGER
+	)`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Sqlite{
+		Db: db,
+	}, nil
+}
+
+func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error) {
+	statement, err := s.Db.Prepare("INSERT INTO students (name, email, age) VALUES (?, ?, ?)")
+	if err != nil {
+		return 0, nil
+	}
+
+	defer statement.Close()
+	res, err := statement.Exec(name, email, age)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return lastId, nil
+}
+
+
+func (s *Sqlite) GetStudentById(id int64) (types.Student, error)  {
+	statement, err := s.Db.Prepare("SELECT id, name, email, age FROM students WHERE id = ? LIMIT 1")
+	if err != nil {
+		return types.Student{}, err
+	} 
+
+	defer statement.Close()
+
+	var student types.Student
+
+	err = statement.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return types.Student{}, fmt.Errorf("no student found with id %s", fmt.Sprint(id))
+		}
+		return types.Student{}, fmt.Errorf("query error: %w", err)
+	}
+
+	return student, nil
+}
+
+func (s *Sqlite) GetAllStudents() ([]types.Student, error) {
+	statement, err := s.Db.Prepare("SELECT id, name, email, age FROM students LIMIT 5")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer statement.Close()
+
+	rows, err := statement.Query()
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next() {
+		var student types.Student
+
+		err := rows.Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+
+		if err != nil {
+			return nil, err
+		}
+
+		students =append(students, student)
+	}
+
+	return students, nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,9 @@
 package storage
 
+import "github.com/iamrohitkandpal/Go-REST-Project/internal/types"
+
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
+	GetStudentById(id int64) (types.Student, error)
+	GetAllStudents() ([]types.Student, error)
 }
