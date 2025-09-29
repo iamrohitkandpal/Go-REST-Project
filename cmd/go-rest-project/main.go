2460cadc27db2a574ca8bdbd5990f439261a9db9
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-09-29 23:54:47 +0530

    Added update and delete functionalities for student records

--- cmd/go-rest-project/main.go
+++ cmd/go-rest-project/main.go
@@ -33,6 +33,8 @@ func main() {
 	router.HandleFunc("POST /api/students", student.New(storage))
 	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
 	router.HandleFunc("GET /api/students/", student.GetAll(storage))
+	router.HandleFunc("PUT /api/students/{id}", student.UpdateOne(storage))
+	router.HandleFunc("DELETE /api/students/{id}", student.DeleteOne(storage))
 
 
 	// Server Setup
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,4 +6,6 @@ type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
 	GetAllStudents() ([]types.Student, error)
+	UpdateStudentById(id int64) (types.Student, error)
+	DeleteStudentById(id int64) (types.Student, error)
 }
